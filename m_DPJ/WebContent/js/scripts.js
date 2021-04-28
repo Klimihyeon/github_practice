1737c6cac7b01848d639d5997a337d95ad300704
Klimihyein <lordnayo@gmail.com>
2021-04-28 18:49:45 +0900

    mid project back

---
+++ m_DPJ/WebContent/js/scripts.js
@@ -0,0 +1,48 @@
+/*!
+    * Start Bootstrap - Agency v6.0.3 (https://startbootstrap.com/theme/agency)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+    */
+    (function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top - 72,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Collapse the navbar when page is scrolled
+})(jQuery); // End of use strict
+    
+    
+    function movePageByGet(url){
+  	  location.href = url;
+    }
+	function logout(url){ 
+		alert("로그아웃 합니다.");
+		movePageByGet(url);
+	}
